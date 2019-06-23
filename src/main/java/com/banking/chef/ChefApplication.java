ffd1f4e24127738ad4d5a8ca98f7ebf285ff0f73
MarekK <kozlowskimarek90@gmail.com>
2019-06-23 13:40:20 +0200

    Initial commit

---
+++ src/main/java/com/banking/chef/ChefApplication.java
@@ -0,0 +1,13 @@
+package com.banking.chef;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChefApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChefApplication.class, args);
+    }
+
+}
