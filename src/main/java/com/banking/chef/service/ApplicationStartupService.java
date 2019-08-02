fa148584bb761fa3d7c22cc1ea7406dd4d05d681
szuwara <kozlowskimarek90@gmail.com>
2019-08-02 22:54:58 +0100

    Trying new way to make service work properly (notifications are sent correctly but exchange rate is downloaded from one day before current day instead of current day - don't know if it's code issue or Heroku server issue -> by the Heroku logs appears that application is restarted multiply times a day and that can be the issue).

--- src/main/java/com/banking/chef/service/ApplicationStartupService.java
+++ src/main/java/com/banking/chef/service/ApplicationStartupService.java
@@ -4,11 +4,21 @@ import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 @Component
 public class ApplicationStartupService implements ApplicationListener<ApplicationReadyEvent> {
+    private long[] values = SMSSenderService.getTimes();
 
     @Override
     public void onApplicationEvent(final ApplicationReadyEvent event) {
-        SMSSenderService.setTimer();
+        long startGetRate = values[0];
+        long startSendNotif = values[1];
+        long delay = values[2];
+        ScheduledExecutorService executor = Executors.newScheduledThreadPool(2);
+        executor.scheduleAtFixedRate(SMSSenderService::getRate, startGetRate, delay, TimeUnit.MILLISECONDS);
+        executor.scheduleAtFixedRate(SMSSenderService::sendSMS, startSendNotif, delay, TimeUnit.MILLISECONDS);
     }
 }
--- src/main/java/com/banking/chef/service/SMSSenderService.java
+++ src/main/java/com/banking/chef/service/SMSSenderService.java
@@ -6,8 +6,6 @@ import com.twilio.type.PhoneNumber;
 
 import java.util.Calendar;
 import java.util.TimeZone;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 class SMSSenderService {
@@ -17,47 +15,103 @@ class SMSSenderService {
     private static final String NUMBER_FROM = System.getenv("TWILIO_NUMBER_FROM");
     private static final String NUMBER_TO = System.getenv("TWILIO_NUMBER_TO");
     private static final TimeZone MY_TIME_ZONE = TimeZone.getTimeZone(System.getenv("TZ"));
-    private static double currentCHFRate = JsonService.readValuesFromJson();
-    private static String smsBody = "Today's CHF exchange rate: " + currentCHFRate;
+    //private static double currentCHFRate = 0;
+    //private static String smsBody = "Today's CHF exchange rate: " + currentCHFRate;
 
 
     static void sendSMS() {
+        double currentCHFRate = getRate();
+        String smsBody = "Today's CHF exchange rate: " + currentCHFRate;
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
         Message message = Message.creator(
                 new PhoneNumber(NUMBER_TO),
                 new PhoneNumber(NUMBER_FROM),
                 smsBody)
                 .create();
-        System.out.println(message.getSid() + "\nNOTIFICATION SENT!");
+        System.out.println(message.getSid() + "\nNOTIFICATION SENT with value of rate: " + currentCHFRate);
     }
 
-    static void setTimer() {
-        Calendar dayOfExecution = Calendar.getInstance();
-        dayOfExecution.setTimeZone(MY_TIME_ZONE);
-        dayOfExecution.set(Calendar.HOUR_OF_DAY, 9);
-        dayOfExecution.set(Calendar.MINUTE, 0);
-        dayOfExecution.set(Calendar.SECOND, 0);
+    static long[] getTimes() {
+        long[] timesArray = new long[3];
+
+        Calendar timeOfGettingRate = Calendar.getInstance();
+        timeOfGettingRate.setTimeZone(MY_TIME_ZONE);
+        timeOfGettingRate.set(Calendar.HOUR_OF_DAY, 8);
+        timeOfGettingRate.set(Calendar.MINUTE, 30);
+        timeOfGettingRate.set(Calendar.SECOND, 0);
+        Calendar timeOfSendingNotif = Calendar.getInstance();
+        timeOfSendingNotif.setTimeZone(MY_TIME_ZONE);
+        timeOfSendingNotif.set(Calendar.HOUR_OF_DAY, 9);
+        timeOfSendingNotif.set(Calendar.MINUTE, 0);
+        timeOfSendingNotif.set(Calendar.SECOND, 0);
+
 
         Calendar currentDay = Calendar.getInstance();
         currentDay.setTimeZone(MY_TIME_ZONE);
 
         long currentTime = currentDay.getTimeInMillis();
-        long delayOfNextNotification = TimeUnit.DAYS.toMillis(1);
-        if (dayOfExecution.getTime().getTime() < currentTime) {
-            dayOfExecution.add(Calendar.DATE, 1);
-            System.out.println("Notification delayed to next day \n" +
-                    "Next occurrence of notification is: " + dayOfExecution.getTime());
+        long delay = TimeUnit.DAYS.toMillis(1);
+
+        if (currentDay.get(Calendar.DAY_OF_WEEK) == Calendar.FRIDAY && currentDay.get(Calendar.HOUR_OF_DAY) > 11) {
+            timeOfGettingRate.add(Calendar.DATE, 3);
+            timeOfSendingNotif.add(Calendar.DATE, 3);
+
+        } else if (currentDay.get(Calendar.DAY_OF_WEEK) == Calendar.SATURDAY) {
+            timeOfGettingRate.add(Calendar.DATE, 2);
+            timeOfSendingNotif.add(Calendar.DATE, 2);
+            System.out.println("Occurrence delayed to " + timeOfGettingRate.getTime());
+        } else if (currentDay.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY) {
+            timeOfGettingRate.add(Calendar.DATE, 1);
+            timeOfSendingNotif.add(Calendar.DATE, 1);
+            System.out.println("Occurrence delayed to " + timeOfGettingRate.getTime());
         }
 
-        long startScheduler = dayOfExecution.getTime().getTime() - currentTime;
+        if (timeOfSendingNotif.getTime().getTime() < currentTime && timeOfGettingRate.getTime().getTime() < currentTime) {
+            timeOfSendingNotif.add(Calendar.DATE, 1);
+            timeOfGettingRate.add(Calendar.DATE, 1);
+            System.out.println("Notification and getting rate delayed to next day \n" +
+                    "Next occurrence of notification is: " + timeOfSendingNotif.getTime() +
+                    "\nNext occurrence of getting rate is: " + timeOfGettingRate.getTime());
+        }
+
+
+        long startNotifScheduler = timeOfSendingNotif.getTime().getTime() - currentTime;
+        long startGetRateScheduler = timeOfGettingRate.getTime().getTime() - currentTime;
+
         {
-            int hours = (int) (startScheduler / 1000 / 60 / 60);
-            int minutes = (int) ((startScheduler / 1000 / 60) % 60);
-            System.out.println(hours + " hours " + minutes + " minutes to pass");
+            int rateDays = getDays(startGetRateScheduler);
+            int rateHours = getHours(startGetRateScheduler);
+            int rateMinutes = getMinutes(startGetRateScheduler);
+            int notifDays = getDays(startNotifScheduler);
+            int notifHours = getHours(startNotifScheduler);
+            int notifMinutes = getMinutes(startNotifScheduler);
+
+            System.out.println(rateDays + " days " + rateHours + " hours " + rateMinutes + " minutes to get rate");
+            System.out.println(notifDays + " days " + notifHours + " hours " + notifMinutes + " minutes to send notification");
         }
 
-        ScheduledExecutorService executor = Executors.newScheduledThreadPool(2);
-        executor.scheduleAtFixedRate(SMSSenderService::sendSMS, startScheduler, delayOfNextNotification, TimeUnit.MILLISECONDS);
+        timesArray[0] = startGetRateScheduler;
+        timesArray[1] = startNotifScheduler;
+        timesArray[2] = delay;
 
+        return timesArray;
+    }
+
+    private static int getMinutes(long source) {
+        return (int) ((source / 1000 / 60) % 60);
+    }
+
+    private static int getHours(long source) {
+        return (int) ((source / 1000 / 60 / 60) % 24);
+    }
+
+    private static int getDays(long source) {
+        return (int) (source / 1000 / 60 / 60 / 24);
+    }
+
+    static double getRate() {
+        double rate = JsonService.readValuesFromJson();
+        System.out.println("Rate downloaded [" + rate + "]");
+        return rate;
     }
 }
