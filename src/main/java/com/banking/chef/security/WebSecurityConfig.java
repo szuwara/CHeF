b7f8ae2af54b22e1f127a47ad41dd0ec625b62e3
szuwara <kozlowskimarek90@gmail.com>
2021-03-17 20:07:07 +0100

    removing login layer

--- src/main/java/com/banking/chef/security/WebSecurityConfig.java
+++ src/main/java/com/banking/chef/security/WebSecurityConfig.java
@@ -1,20 +1,22 @@
 package com.banking.chef.security;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
+        {
+            http.httpBasic().disable();
+        }
+    /*
         http
                 .authorizeRequests()
                 .antMatchers("/home", "/test").permitAll()
@@ -26,8 +28,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .logout()
                 .permitAll();
+                */
     }
 
+
+    /*
     @Bean
     @Override
     public UserDetailsService userDetailsService() {
@@ -47,4 +52,5 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
         return new InMemoryUserDetailsManager(user, admin);
     }
+    */
 }
